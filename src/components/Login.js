6f8ca074e56da333104546914e5479716235271a
Aleksis <aleksis.canlas207@gmail.com>
2021-03-30 07:33:43 +0800

    testtt

--- src/components/Login.js
+++ src/components/Login.js
@@ -49,7 +49,7 @@ export default function Login() {
             <Form.Control type="email" ref={emailRef} required />
           </Form.Group>
           <Form.Group id="password">
-            <Form.Label>Passworrrrrrrrrrrd</Form.Label>
+            <Form.Label>Passworrrrrrrrrrrrrrd</Form.Label>
             <Form.Control type="password" ref={passwordRef} required />
           </Form.Group>
           <Button disabled={loading} className="w-100" type="submit">
